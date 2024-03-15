2be6ba4929873e2c36891f60172ffcf205014719
Raul henrique <121063862+raulcacula@users.noreply.github.com>
2024-03-15 20:46:48 -0300

    TheVideos

---
+++ index.js
@@ -0,0 +1,38 @@
+var pesq = document.getElementById('pesquise')
+var barra = document.querySelector('.items-pesquisa-mobile')
+
+function barraPesq(){
+    if(barra.style.display = 'flex'){
+
+    }
+}
+
+var fechar = document.getElementById('fechar-pesq')
+
+function fecharPesq(){
+    if(fechar = barra.style.display = 'none'){
+    }
+}
+
+fechar.addEventListener('click', fecharPesq)
+
+
+var btnCriar = document.getElementById('btn-criar')
+var pagina = document.querySelector('.conta')
+
+function abrirConta(){
+    if(btnCriar =  pagina.style.display = 'flex'){
+    }
+}
+ btnCriar.addEventListener('click', abrirConta)
+
+
+ var fecharLogar = document.getElementById('fechar-aba')
+
+ function fecharConta(){
+    if(fecharLogar = pagina.style.display = 'none'){
+
+    }
+ }
+
+fecharLogar.addEventListener('click', fecharConta)
